e6658363915b7b7532bd6709e0a31cf5ae843b74
eric-prog <eric.sprog@gmail.com>
2023-02-28 15:23:21 -0500

    API pagination

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import { useEffect, useState } from "react";
+import Posts from "./components/Posts";
+import Pagination from "./components/Pagination";
 import './App.css';
 
 function App() {
+  const [posts, setPosts] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [currentPage, setCurrentPage] = useState(1)
+  const [postsPerPage] = useState(10)
+
+  useEffect(() => {
+    const getData = async () => {
+      setLoading(true)
+      const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+      const responseData = await response.json()
+      setPosts(responseData)
+      setLoading(false)
+    }
+
+    getData()
+  }, []) // without dependencies it would loop forever
+
+  // get posts
+  const indexOfLastPost = currentPage * postsPerPage
+  const indexOfFirstPost = indexOfLastPost - postsPerPage
+  const currentPost = posts.slice(indexOfFirstPost, indexOfLastPost)
+
+  const paginate = (number) => {
+    setCurrentPage(number)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <Posts loading={loading} posts={currentPost} />
+       <Pagination postsPerPage={postsPerPage} totalPosts={posts.length} paginate={paginate} />
     </div>
   );
 }
